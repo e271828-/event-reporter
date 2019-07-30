e553fc31b905b67235a3aaf23fd099625051a8ef
Alex Newman <posix4e@gmail.com>
2019-07-29 17:32:18 -0700

    yapf tests

--- tests/test_er.py
+++ tests/test_er.py
@@ -5,6 +5,7 @@ from mockredis.exceptions import ResponseError
 
 import os
 
+
 class EventReporterTest(unittest.TestCase):
     def setUp(self):
         self.conn = WithRedis.StrictRedis()
@@ -30,7 +31,16 @@ class EventReporterTest(unittest.TestCase):
         """
         Checks to see that the EventReporter stores expected data
         """
-        ar = self.er.store('ga', 'event', '20538abc-a8af-46e0-b292-0999d94468e9', category='user', action='action_name', aip='1', uip='1.2.3.4', ds='web', ua='my-useragent-test')
+        ar = self.er.store(
+            'ga',
+            'event',
+            '20538abc-a8af-46e0-b292-0999d94468e9',
+            category='user',
+            action='action_name',
+            aip='1',
+            uip='1.2.3.4',
+            ds='web',
+            ua='my-useragent-test')
 
         self.assertTrue(ar == None)
 
@@ -63,13 +73,28 @@ class EventReporterTest(unittest.TestCase):
 
         # print(f'Data has been sent to {self.my_ua}. Please check real-time stats to confirm correctness.')
 
-
     def test_store_fetch_oldest_double(self):
         """
         Checks to see that the EventReporter fetch_oldest gets expected data
         """
-        ar = self.er.store('ga', 'event', '20538abc-a8af-46e0-b292-0999d94468e9', category='user', action='action_name', aip='1', uip='1.2.3.4', ds='web')
-        ar2 = self.er.store('ga', 'event', '20538abc-a8af-46e0-b292-0999d94468e9', category='user', action='action_name_2', aip='1', uip='1.2.3.4', ds='web')
+        ar = self.er.store(
+            'ga',
+            'event',
+            '20538abc-a8af-46e0-b292-0999d94468e9',
+            category='user',
+            action='action_name',
+            aip='1',
+            uip='1.2.3.4',
+            ds='web')
+        ar2 = self.er.store(
+            'ga',
+            'event',
+            '20538abc-a8af-46e0-b292-0999d94468e9',
+            category='user',
+            action='action_name_2',
+            aip='1',
+            uip='1.2.3.4',
+            ds='web')
 
         self.assertTrue(ar == None)
 
@@ -95,13 +120,20 @@ class EventReporterTest(unittest.TestCase):
         del expected['ts']
 
         self.assertDictContainsSubset(expected, r)
-
 
     def test_store_fetch_oldest_single(self):
         """
         Checks to see that the EventReporter fetch_oldest gets expected data
         """
-        ar = self.er.store('ga', 'event', '20538abc-a8af-46e0-b292-0999d94468e9', category='user', action='action_name', aip='1', uip='1.2.3.4', ds='web')
+        ar = self.er.store(
+            'ga',
+            'event',
+            '20538abc-a8af-46e0-b292-0999d94468e9',
+            category='user',
+            action='action_name',
+            aip='1',
+            uip='1.2.3.4',
+            ds='web')
 
         self.assertTrue(ar == None)
 
@@ -127,34 +159,61 @@ class EventReporterTest(unittest.TestCase):
         del expected['ts']
 
         self.assertDictContainsSubset(expected, r)
-
 
     def test_unsafe_store(self):
         '''
         Verify that e.g. a redis or argument failure throws an exception.
         '''
         with self.assertRaises(TypeError):
-            self.er.store(category='user', action='action_name', aip='1', uip='1.2.3.4', ds='web')
+            self.er.store(
+                category='user',
+                action='action_name',
+                aip='1',
+                uip='1.2.3.4',
+                ds='web')
 
     def test_safe_store_fail(self):
         '''
         Verify that e.g. a redis or argument failure does not throw an exception.
         '''
-        r = self.er.safe_store(None, None, None, action='action_name', aip='1', uip='1.2.3.4', ds='web')
+        r = self.er.safe_store(
+            None,
+            None,
+            None,
+            action='action_name',
+            aip='1',
+            uip='1.2.3.4',
+            ds='web')
         self.assertTrue(r == False)
 
     def test_safe_store_success(self):
         '''
         Verify that e.g. a redis or argument failure does not throw an exception.
         '''
-        r = self.er.safe_store('ga', 'event', '20538abc-a8af-46e0-b292-0999d94468e9', category='user', action='action_name', aip='1', uip='1.2.3.4', ds='web')
+        r = self.er.safe_store(
+            'ga',
+            'event',
+            '20538abc-a8af-46e0-b292-0999d94468e9',
+            category='user',
+            action='action_name',
+            aip='1',
+            uip='1.2.3.4',
+            ds='web')
         self.assertTrue(r == None)
 
     def test_safe_store_success_honey(self):
         '''
         Verify that e.g. a redis or argument failure does not throw an exception.
         '''
-        r = self.er.safe_store('honey', 'event', '20538abc-a8af-46e0-b292-0999d94468e9', category='user', action='action_name', aip='1', uip='1.2.3.4', ds='web')
+        r = self.er.safe_store(
+            'honey',
+            'event',
+            '20538abc-a8af-46e0-b292-0999d94468e9',
+            category='user',
+            action='action_name',
+            aip='1',
+            uip='1.2.3.4',
+            ds='web')
         self.assertTrue(r == None)
 
     def test_store_fetch_dispatch_referrer(self):
@@ -162,7 +221,16 @@ class EventReporterTest(unittest.TestCase):
         Checks to see that the EventReporter stores expected data with referrer.
         Looks like it's unnecessary to urlencode prior to handing it off.
         """
-        ar = self.er.store('ga', 'event', '20538abc-a8af-46e0-b292-0999d94468e9', category='user', action='action_name', aip='1', uip='1.2.3.4', ds='web', dr='http://www.test.com')
+        ar = self.er.store(
+            'ga',
+            'event',
+            '20538abc-a8af-46e0-b292-0999d94468e9',
+            category='user',
+            action='action_name',
+            aip='1',
+            uip='1.2.3.4',
+            ds='web',
+            dr='http://www.test.com')
 
         self.assertTrue(ar == None)
 
